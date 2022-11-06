5c7f89ec59cccfd090981c285e2ab2ec8423bdeb
mpekcan <mpekcan@atlassian.com>
2022-11-06 23:39:54 +0300

    Add 03-fonts lesson

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,22 @@
+import { Inter } from "@next/font/google";
+
 import styles from "./styles.module.css";
 
 import "../styles/global.css";
 
+const interFont = Inter();
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="tr">
+    <html lang="tr" className={interFont.className}>
       <body className={styles.container}>
-        <header>Header</header>
+        <header>
+          <p>Header</p>
+        </header>
         <main>{children}</main>
-        <footer>Footer</footer>
+        <footer>
+          <p>Footer</p>
+        </footer>
       </body>
     </html>
   );
