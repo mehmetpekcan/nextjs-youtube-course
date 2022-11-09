7b19bcf41cf0fd03670299dabfa29eae7b6813a3
mpekcan <mpekcan@atlassian.com>
2022-11-09 18:42:41 +0300

    Fetch posts

--- app/page.js
+++ app/page.js
@@ -1,9 +1,19 @@
+import styles from "./styles.module.css";
 import BlogCard from "../components/BlogCard";
 
-export default function Home() {
+async function getPosts() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  return response.json();
+}
+
+export default async function Home() {
+  const { posts } = await getPosts();
+
   return (
-    <div>
-      <BlogCard />
+    <div className={styles.blogsContainer}>
+      {posts.map((post) => (
+        <BlogCard key={post.id} {...post} />
+      ))}
     </div>
   );
 }
--- components/BlogCard/index.js
+++ components/BlogCard/index.js
@@ -4,15 +4,19 @@ import Image from "next/image";
 
 import styles from "./styles.module.css";
 
-function BlogCard() {
+function BlogCard({ title, body, id }) {
   return (
     <Link href="/" className={styles.card}>
       <div className={styles.cardImage}>
-        <Image src="https://picsum.photos/200/300" alt="Blog Card Image" fill />
+        <Image
+          src={`https://picsum.photos/200/300?random=${id}`}
+          alt="Blog Card Image"
+          fill
+        />
       </div>
       <div className={styles.cardBody}>
-        <h3>Title</h3>
-        <p>Lorem ipsum</p>
+        <h3>{title}</h3>
+        <p>{body}</p>
       </div>
     </Link>
   );
